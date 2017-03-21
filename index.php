0189307482c921b271ffe2703d7165a61ff009ef
Ian Leonard <ifl2@pitt.edu>
2017-03-21 15:26:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
+	if (session_status() == PHP_SESSION_NONE)
+		session_start();
 
-if (session_status() == PHP_SESSION_NONE)
-	session_start();
-function __autoload($class) {
-	$class = strtolower($class);
-	$file = $class . '.php';
-	if (file_exists($file))
-		include $file;
-}
+	function __autoload($class) {
+		$class = strtolower($class);
+		$file = $class . '.php';
+		if (file_exists($file))
+			include $file;
+	}
 
-class MyDB extends SQLite3 {
-	function __construct() {
-		$this->open('sample.db');}
-}
+	class MyDB extends SQLite3 {
+		function __construct() {
+			$this->open('sample.db');}
+	}
 
-$db = new MyDB();
-if(!$db)
-  echo $db->lastErrorMsg();
+	$db = new MyDB();
+	if(!$db)
+	  echo $db->lastErrorMsg();
 
-error_reporting(E_ALL);
-
+	error_reporting(E_ALL);
 ?>
 
 <!DOCTYPE html>
@@ -47,7 +46,6 @@ error_reporting(E_ALL);
 	  </div>
 	</div>
 	
-	
     <table class="sortable table-fill">
       <thead><tr>
         <th>Job</th>
@@ -60,24 +58,24 @@ error_reporting(E_ALL);
       </tr></thead>
 	  <tbody>
 		<?php 
-		$sql ="SELECT * from JOBS";
-		$ret = $db->query($sql);
-		while($row = $ret->fetchArray(SQLITE3_ASSOC)) {
-			$jid = $row["id"];
-			$name = $row["description"];
-			$dataset = $row["type"];
-			$status = $row["completed"];
-			$starttime = $row["startTime"];
-			echo "<td>$jid</td>";
-			echo "<td>$name</td>";
-			echo "<td>$dataset</td>";
-			echo "<td>$status</td>";
-			echo "<td>$starttime</td>";
-			echo "<td></td>";
-			echo "<td></td>";
-			echo "</tr>";
-		}
-		$db->close();
+			$sql ="SELECT * from JOBS";
+			$ret = $db->query($sql);
+			while($row = $ret->fetchArray(SQLITE3_ASSOC)) {
+				$jid = $row["id"];
+				$name = $row["description"];
+				$dataset = $row["type"];
+				$status = $row["completed"];
+				$starttime = $row["startTime"];
+				echo "<td>$jid</td>";
+				echo "<td>$name</td>";
+				echo "<td>$dataset</td>";
+				echo "<td>$status</td>";
+				echo "<td>$starttime</td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "</tr>";
+			}
+			$db->close();
 		?>
 	</tbody>
   </table>
